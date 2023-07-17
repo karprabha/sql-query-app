65879b8697e49d9e8c39f5384ebe592fcb2a648d
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-07-18 02:18:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>Hello World</h1>
+            </div>
+            <p>This is paragraph</p>
+        </>
+    );
+}
+
+export default App;
