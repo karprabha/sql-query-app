5669125a88c8328bf2e92fcebf0a6cc424b39e19
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-07-20 02:57:02 +0530

    add query search feature

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import QueryInput from "../components/QueryInput";
+import ResultTable from "../components/ResultTable";
+import getQueryData from "../utils/getQueryData";
+
 const Home = () => {
-    return <h1>This is Home</h1>;
+    const [results, setResults] = useState(null);
+
+    const handleExecuteQuery = (query) => {
+        const fetchedResults = getQueryData(query);
+        setResults(fetchedResults);
+    };
+
+    const handleClearResults = () => {
+        setResults(null);
+    };
+
+    return (
+        <div className="home-container">
+            <QueryInput
+                onExecute={handleExecuteQuery}
+                onClear={handleClearResults}
+            />
+            {results && <ResultTable data={results} />}{" "}
+        </div>
+    );
 };
 
 export default Home;
