0e6a908ca93b156a4c27584e3dbd82739f264f8b
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-07-20 14:00:09 +0530

    add local storage for queryHistory

--- src/routes/History.jsx
+++ src/routes/History.jsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from "react";
+
 const History = () => {
-    return <h1>This is History</h1>;
+    const [historyList, setHistoryList] = useState([]);
+
+    useEffect(() => {
+        const historyData = JSON.parse(localStorage.getItem("history")) || [];
+
+        const sortedHistory = historyData.slice().sort((a, b) => {
+            const dateA = a.timestamp.split(" ")[0];
+            const dateB = b.timestamp.split(" ")[0];
+            const timeA = a.timestamp.split(" ")[1];
+            const timeB = b.timestamp.split(" ")[1];
+
+            return dateB.localeCompare(dateA) || timeB.localeCompare(timeA);
+        });
+
+        setHistoryList(sortedHistory);
+    }, []);
+
+    return (
+        <div className="history-container">
+            <h2>Query History</h2>
+            <ul>
+                {historyList.map((item, index) => (
+                    <li key={index}>
+                        <div>
+                            <strong>Query:</strong> {item.query}
+                        </div>
+                        <div>
+                            <strong>Timestamp:</strong> {item.timestamp}
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default History;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import QueryInput from "../components/QueryInput";
 import ResultTable from "../components/ResultTable";
 import getQueryData from "../utils/getQueryData";
 
 const Home = () => {
     const [results, setResults] = useState(null);
+    const [executedQuery, setExecutedQuery] = useState({});
 
     const handleExecuteQuery = (query) => {
         const fetchedResults = getQueryData(query);
         setResults(fetchedResults);
+
+        const timestamp = new Date().toLocaleString();
+        setExecutedQuery({ query, timestamp });
     };
+
+    useEffect(() => {
+        if (!executedQuery.query) return;
+        const historyData = JSON.parse(localStorage.getItem("history")) || [];
+        const updatedHistory = [...historyData, executedQuery];
+        localStorage.setItem("history", JSON.stringify(updatedHistory));
+    }, [executedQuery]);
 
     const handleClearResults = () => {
         setResults(null);
