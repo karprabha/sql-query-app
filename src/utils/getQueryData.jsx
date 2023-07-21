51fd1b2d58070ff859b85ade23a1e90c8f529165
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-07-21 23:36:54 +0530

    add large dataset

--- src/utils/getQueryData.jsx
+++ src/utils/getQueryData.jsx
@@ -1,5 +1,6 @@
 import employeeData from "../data/__mocks__/employeeData.json";
 import studentRecords from "../data/__mocks__/studentRecords.json";
+import productData from "../data/__mocks__/productData.json";
 
 const getQueryData = (query) => {
     switch (query) {
@@ -7,6 +8,8 @@ const getQueryData = (query) => {
             return employeeData;
         case "q2":
             return studentRecords;
+        case "q3":
+            return productData;
         default:
             return null;
     }
